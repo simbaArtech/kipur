2c3a51fd06373d23eabca654d26f07becd909cad
Danieli <danielitabach@gmail.com>
2023-04-24 17:21:28 +0300

    second commit

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,14 @@
+import { Wrapper } from "./styles"
+
+export default function  Navbar () {
+    return (
+        <Wrapper>
+            <div>מומלצים</div>
+            <div>אישים וחללים</div>
+            <div>אירועים</div>
+            <div>תמונות מספרות</div>
+            <div>יחידות אמ"ן</div>
+            <div>ציר זמן</div>
+        </Wrapper>
+    )
+}
---
+++ src/components/navbar/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+
+export const Wrapper = styled.div`
+    background: #20211E;
+    color: white;
+    display: flex;
+    justify-content: space-around;
+    padding: 1rem 0 1rem 0;
+`
