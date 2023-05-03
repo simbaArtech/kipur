48d3e379891737edfc8cba0e397890221eadb9b5
Danieli <danielitabach@gmail.com>
2023-05-03 09:43:32 +0300

    build removed routing

--- src/components/header/navbar/Navbar.js
+++ src/components/header/navbar/Navbar.js
@@ -1,22 +1,13 @@
 import { Wrapper } from "./styles";
-import { useState } from "react";
 import styled from "styled-components";
-import { useNavigate } from 'react-router-dom';
-import { useEffect } from "react";
 
 export default function Navbar({ selected, setSelected }) {
-
-    let navigate = useNavigate();
 
     const Item = styled.div`
         ${({ isSelected }) => isSelected && `
         color: #99b882;
         border-bottom: 2.5px solid #99b882;
     `}`;
-
-    useEffect(() => {
-        navigate(`/${selected}`);
-    }, [selected]);
 
     const handleChange = (name) => {
         setSelected(name);
