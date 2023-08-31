36a97b720733e9944fd8cf57fe49dcfc1b21651f
arlinka-a <arlin.agarunov@gmail.com>
2023-08-31 13:40:15 +0300

    changes for today

--- src/pages/timeline/TimeLine.js
+++ src/pages/timeline/TimeLine.js
@@ -1,32 +1,61 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import {
-  ImageScroller,
-  ImageContainer,
   Image,
   StyledCarousel,
-  SelectedImage,
   ImageTitle,
+  ImageDate,
+  Container
 } from "./TimeLineStyles";
 import { data } from "./consts";
-import { consts } from "react-elastic-carousel";
 
 export default function TimeLine() {
   const [selectedItem, setSelectedItem] = useState(0);
+  const [style, setStyle] = useState();
+  // const carouselRef = useRef();
+  const imageRef = useRef(null);
 
   const handleChangeImage = (currentItem) => {
     setSelectedItem(currentItem.index);
+    // carouselRef.current.goTo(currentItem.index);
   };
 
+  useEffect(() => {
+    if (imageRef) {
+      const imageRect = imageRef.current.getBoundingClientRect();
+      const top = imageRect.top + window.scrollY + imageRect.height / 2;
+      const left = imageRect.left + window.scrollX + imageRect.width / 2;
+      setStyle({ top: `${top}px`, left: `${left}px` });
+    }
+  }, [selectedItem]);
+
   return (
-    <StyledCarousel
-      verticalMode
-      itemsToShow={4}
-      focusOnSelect={true}
-      onChange={(event) => handleChangeImage(event)}
-    >
-      {data.map((item, index) => (
-        <Image src={item.pic} isSelected={index === selectedItem} />
-      ))}
-    </StyledCarousel>
+    <Container>
+      <StyledCarousel
+        verticalMode
+        itemsToShow={3}
+        // preventDefaultTouchmoveEvent
+        // ref={carouselRef}
+        focusOnSelect={true}
+        onChange={(event) => handleChangeImage(event)}
+      >
+        {data.map((item, index) => (
+          <>
+            <Image
+              src={item.pic}
+              isSelected={index === selectedItem}
+              ref={imageRef}
+            />
+            {index === selectedItem ? (
+              <div>
+                <ImageDate style={style}>{item.date}</ImageDate>
+                <ImageTitle style={style}>{item.title}</ImageTitle>
+              </div>
+            ) : (
+              ""
+            )}
+          </>
+        ))}
+      </StyledCarousel>
+    </Container>
   );
 }
--- src/pages/timeline/TimeLineStyles.js
+++ src/pages/timeline/TimeLineStyles.js
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 import Carousel from "react-elastic-carousel";
 
+export const Container = styled.div`
+  display: flex;
+`;
+
 export const StyledCarousel = styled(Carousel)`
   .rec.rec-arrow {
     display: none;
@@ -8,14 +12,6 @@ export const StyledCarousel = styled(Carousel)`
   .rec.rec-pagination {
     display: none;
   }
-`;
-
-export const ImageScroller = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 100%;
-  height: 100%;
 `;
 
 export const ImageContainer = styled.div`
@@ -29,22 +25,17 @@ export const Image = styled.img`
   border: ${({ isSelected }) => (isSelected ? "2px solid white" : "")};
   margin-bottom: 1rem;
   border-radius: 10px;
+  height: 200px;
   opacity: ${({ isSelected }) => (isSelected ? "1" : "0.5")};
   transition: opacity 0.3s ease-in-out;
 `;
 
-export const SelectedImage = styled.img`
-  width: 250px;
-  opacity: 1;
-`;
-
 export const ImageTitle = styled.p`
   position: absolute;
-  transform: translate(-50%, -50%);
   color: white;
-  padding: 10px;
-  border-radius: 5px;
 `;
-
-// top: 50%;
-// left: 50%;
+export const ImageDate = styled.p`
+  position: absolute;
+  font-family: "Almoni";
+  color: #797979;
+`;
