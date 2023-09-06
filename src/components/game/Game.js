aef3086f133cf0afa1b7a411faef1d5c44e92c9d
Tal <tgoren01@gmail.com>
2023-09-06 13:54:12 +0300

    added show data state

--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -6,169 +6,169 @@ import { Button } from "bootstrap";
   
   const CLUE_DATA = {
     'Ac1': {
-      clue: 'דגן יצטרך (3,4)',
-      answer: 'aaaaaaa',
+      clue: 'אחרי המשחק יש שידור חוזר של אלוף פיקוד דרום בדימוס (3, 4)',
+      answer: 'גורודיש',
       direction: 'across',
       number: 1,
       boxes: ['A12', 'A11', 'A10', 'A9', 'A8', 'A7', 'A6']
     },
     'Ac4': {
-      clue: 'איבר פנימי (4)',
-      answer: 'aaaa',
+      clue: 'תשבעי ממערב או ממזרח אלינו? (4)',
+      answer: 'ירדנ',
       direction: 'across',
       number: 4,
       boxes: ['A4', 'A3', 'A2', 'A1']
     },
     'Ac7': {
-      clue: 'כשהפרות לא  (4)',
-      answer: 'aaaa',
+      clue: 'מקצועו של אגרנט היה פשוט טרוף! (4)',
+      answer: 'שופט',
       direction: 'across',
       number: 7,
       boxes: ['C12', 'C11', 'C10', 'C9']
     },
     'Ac8': {
-      clue: 'עני מוקף (7)',
-      answer: 'aaaaaaa',
+      clue: 'מה עשו אולי העובדים בחנות התרמילים לידיד משכבר הימים? (3, 4)',
+      answer: 'מכרותיק',
       direction: 'across',
       number: 8,
       boxes: ['C7', 'C6', 'C5', 'C4', 'C3', 'C2', 'C1']
     },
     'Ac10': {
-      clue: 'מסרו את הגופה (6)',
-      answer: 'aaaaaa',
+      clue: 'דוד אלעזר, למשל, נלחם בנוגע למנת קרב (2, 4)',
+      answer: 'רבאלופ',
       direction: 'across',
       number: 10,
       boxes: ['E12', 'E11', 'E10', 'E9', 'E8', 'E7']
     },
     'Ac12': {
-      clue: 'חיה יונקת (5)',
-      answer: 'aaaaa',
+      clue: 'מכנים אותו "המלאך" וראמ"ן נטרף! (5)',
+      answer: 'מרואנ',
       direction: 'across',
       number: 12,
       boxes: ['F5', 'F4', 'F3', 'F2', 'F1']
     },
     'Ac13': {
-      clue: 'העז שלי (5)',
-      answer: 'aaaaa',
+      clue: 'בבית החולים ליולדות יש כוח שצריך להביא לצבא (5)',
+      answer: 'לגייס',
       direction: 'across',
       number: 12,
       boxes: ['G12', 'G11', 'G10', 'G9', 'G8']
     },
     'Ac15': {
-      clue: 'סוג של (6)',
-      answer: 'aaaaaa',
+      clue: 'באיראן החרימו את הכוחות המיוחדים (6)',
+      answer: 'קומנדו',
       direction: 'across',
       number: 15,
       boxes: ['H6', 'H5', 'H4', 'H3', 'H2', 'H1']
     },
     'Ac18': {
-      clue: 'ברומניה אפשר (7)',
-      answer: 'aaaaaaa',
+      clue: 'ראש המוסד נולד בארץ והוגה לי רעיונות בפנים (3, 4)',
+      answer: 'צביזמיר',
       direction: 'across',
       number: 18,
       boxes: ['J12', 'J11', 'J10', 'J9', 'J8', 'J7', 'J6']
     },
     'Ac19': {
-      clue: 'עם 3 מאונך (2, 2)',
-      answer: 'aaaa',
+      clue: 'תעלה אחרי עשר, הוא ממהר! (4)',
+      answer: 'סואצ',
       direction: 'across',
       number: 19,
       boxes: ['J4', 'J3', 'J2', 'J1']
     },
     'Ac20': {
-      clue: 'הסכים לקבל את (4)',
-      answer: 'aaaa',
+      clue: 'כשהוא מגיע מוקף בשיירת אוניות זה קשור אולי לצה"ל (4)',
+      answer: 'צבאי',
       direction: 'across',
       number: 20,
       boxes: ['L12', 'L11', 'L10', 'L9']
     },
     'Ac21': {
-      clue: 'החיידק סופר עבורה (7)',
-      answer: 'aaaaaaa',
+      clue: 'שר הביטחון באיראן יכול לשכר אותך (3, 4)',
+      answer: 'משהדיינ',
       direction: 'across',
       number: 21,
       boxes: ['L7', 'L6', 'L5', 'L4', 'L3', 'L2', 'L1']
     },
     'Dn1': {
-      clue: 'בני הזוג לשעבר (5)',
-      answer: 'aaaaa',
+      clue: 'כשנוכרי מקיף נוצרי זה יכול לפתור סכסוך (5)',
+      answer: 'גישור',
       direction: 'down',
       number: 1,
       boxes: ['A12', 'B12', 'C12', 'D12', 'E12']
     },
     'Dn2': {
-      clue: 'א אוהל (5)',
-      answer: 'aaaaa',
+      clue: 'בזנב האווירון יש כלי נשק שסתום (5)',
+      answer: 'ונטיל',
       direction: 'down',
       number: 2,
       boxes: ['A9', 'B9', 'C9', 'D9', 'E9']
     },
     'Dn3': {
-      clue: 'ראה 19 מאוזן (5)',
-      answer: 'aaaaa',
+      clue: 'הוא יגדל ויתפתח ואני הבנתי והכנסתי לראש בלי ראש על הראש (5)',
+      answer: 'יתמנפ',
       direction: 'down',
       number: 3,
       boxes: ['A7', 'B7', 'C7', 'D7', 'E7']
     },
     'Dn5': {
-      clue: 'הזמרת תזיז (4, 4)',
-      answer: 'aaaaaaaa',
+      clue: 'מותר להגן על המקום שהסורים ניסו לכבוש (3, 5)',
+      answer: 'רמתהגולנ',
       direction: 'down',
       number: 5,
       boxes: ['A3', 'B3', 'C3', 'D3', 'E3', 'F3', 'G3', 'H3']
     },
     'Dn6': {
-      clue: 'חצבו באבן (6)',
-      answer: 'aaaaaa',
+      clue: 'מי ששולח לישראל רכבת אווירית מבריח רוצח מלמטה (6)',
+      answer: 'ניקסונ',
       direction: 'down',
       number: 6,
       boxes: ['A1', 'B1', 'C1', 'D1', 'E1', 'F1']
     },
     'Dn9': {
-      clue: 'ארגון הכדורגל (4)',
-      answer: 'aaaa',
+      clue: 'הוא נראה בלי זנבו כשנישא מסביב וזה היה התפקיד של גולדה [ראשי תיבות] (4)',
+      answer: 'רוהמ',
       direction: 'down',
       number: 9,
       boxes: ['C5', 'D5', 'E5', 'F5']
     },
     'Dn11': {
-      clue: 'חוללה פלאים (4, 4)',
-      answer: 'aaaaaaaa',
+      clue: 'ראש אמ"ן לשעבר מכניס חצי ליטר קטן באלפים (3, 5)',
+      answer: 'אליזעירא',
       direction: 'down',
       number: 11,
       boxes: ['E10', 'F10', 'G10', 'H10', 'I10', 'J10', 'K10', 'L10']
     },
     'Dn13': {
-      clue: 'מצער מאוד (6)',
-      answer: 'aaaaaa',
+      clue: 'הצי צלף כשהתבקש להפגיז (6)',
+      answer: 'להפציצ',
       direction: 'down',
       number: 13,
       boxes: ['G12', 'H12', 'I12', 'J12', 'K12', 'L12']
     },
     'Dn14': {
-      clue: 'ישתמש בגרונו (4)',
-      answer: 'aaaa',
+      clue: 'חוסיין - יש לו מכשיר עינויים לפני (4)',
+      answer: 'סדאמ',
       direction: 'down',
       number: 14,
       boxes: ['G8', 'H8', 'I8', 'J8']
     },
     'Dn15': {
-      clue: 'אני מבולבל (5)',
-      answer: 'aaaaa',
+      clue: 'לב מצרי במבצע שאפשר לראות בשדה! (5)',
+      answer: 'קצרדש',
       direction: 'down',
       number: 15,
       boxes: ['H6', 'I6', 'J6', 'K6', 'L6']
     },
     'Dn16': {
-      clue: 'יכינו אוכל (5)',
-      answer: 'aaaaa',
+      clue: 'קצין אפס מעל המצבה נוחל כישלון (5)',
+      answer: 'מפסיד',
       direction: 'down',
       number: 16,
       boxes: ['H4', 'I4', 'J4', 'K4', 'L4']
     },
     'Dn17': {
-      clue: 'התבאסה כי (5)',
-      answer: 'aaaaa',
+      clue: 'היה נשיא מצויין! (5)',
+      answer: 'ויצמנ',
       direction: 'down',
       number: 17,
       boxes: ['H1', 'I1', 'J1', 'K1', 'L1']
@@ -184,18 +184,27 @@ import { Button } from "bootstrap";
         clues: CLUE_DATA,
         activeClueBoxes: CLUE_DATA['Ac1'].boxes,
         activeClue: ['Ac1'],
-        boxInFocus: CLUE_DATA['Ac1'].boxes[0]
+        boxInFocus: CLUE_DATA['Ac1'].boxes[0],
+        showData: false
       };
       
       this.setActiveClueBoxes = this.setActiveClueBoxes.bind(this);
       this.setActiveClue = this.setActiveClue.bind(this);
       this.setBoxInFocus = this.setBoxInFocus.bind(this);
+      this.setShowData = this.setShowData.bind(this);
     }
     
     setActiveClueBoxes(boxes) {
       this.setState({
         activeClueBoxes: boxes
       });
+    }
+    setShowData() {
+      this.setState({
+        showData: true
+      });
+      
+      console.log(this.state.showData)
     }
     
     setActiveClue(clue) {
@@ -226,9 +235,11 @@ import { Button } from "bootstrap";
                     boxes.map((cell) => {
                       inputWord += document.getElementById(`input${cell}`).value;
                     })
+                    console.log(inputWord)
                     if (inputWord === clue.answer) {
                       boxes.map((cell) => {
-                        document.getElementById(`input${cell}`).setAttribute("backgroundColor", "green");
+                        document.getElementById(`input${cell}`).classList.add("right");
+                        document.getElementById(`input${cell}`).classList.remove("highlight");
                       })
                     }
                 }
@@ -237,9 +248,6 @@ import { Button } from "bootstrap";
             }
             
           }
-          // if(box.letter === data.value)
-
-          //   console.log("hi")
         }
         })
       }
@@ -252,6 +260,7 @@ import { Button } from "bootstrap";
           <Board grid={ this.state.grid } allClues={ this.state.clues } clues={ this.state.clues } setActiveClueBoxes={ this.setActiveClueBoxes } highlightedBoxes={ this.state.activeClueBoxes } setActiveClue = { this.setActiveClue } setBoxInFocus={ this.setBoxInFocus } boxInFocus={ this.state.boxInFocus }/>
           <Clues clues={ this.state.clues } setActiveClueBoxes={ this.setActiveClueBoxes } activeClue={ this.state.activeClue } setActiveClue={ this.setActiveClue } setBoxInFocus={ this.setBoxInFocus } />
           <button onClick={this.handleCheckAnswers} className="buttonCheck">בדיקה</button>
+          <button onClick={this.setShowData} className="buttonCheck">חשיפת התשובות</button>
           {/* <div className="text">לחצו על המשבצות והתחילו בתשבץ</div> */}
         </div>
       );
@@ -401,7 +410,7 @@ import { Button } from "bootstrap";
       }
       
       if (this.props.letter) {  
-        input = <input id={`input${this.props.id}`} type="text" maxLength="1" className={ `box-input ${this.state.highlight ? 'highlight' : ''}` } onFocus={ this.handleFocus } ref={(input) => { this.textInput = input }} />
+        input = <input id={`input${this.props.id}`} value={this.showData && this.props.letter} type="text" maxLength="1" className={ `box-input ${this.state.highlight ? 'highlight' : ''}` } onFocus={ this.handleFocus } ref={(input) => { this.textInput = input }} />
       }
       
      return (
